56a8c699e5a09af3c505619e3af045651695e396
medsonmoombe <medsonmoombe21@gmail.com>
2022-05-05 15:45:07 +0200

    Add the the index.js main function

--- src/index.js
+++ src/index.js
@@ -1,50 +1,17 @@
+
 import './index.css';
-import List from './todo-list.js';
-
-
-const todoInput = document.querySelector('.todo-input');
-// const checkBox = document.querySelector('#check');
-// const myTodo = document.querySelector('.my-todo');
-// const optionIcon = document.querySelector('.fa-ellipsis-vertical');
-// const deleteItem = document.querySelector('.show');
-// const clearAllBtn = document.querySelector('.clearAll');
-const enterKey = document.querySelector('.down-left');
-// const todoDisplay = document.querySelector('.todos-display');
-// const labelTag = document.getElementsByTagName('label');
-// const show = document.querySelector('.show');
-
-
-
-
-
-
-
-let todos = JSON.parse(localStorage.getItem('my-todo')) !== null ? (todos = JSON.parse(localStorage.getItem('my-todo'))) : [];
-let editId;
-let ifEditedTask = false;
-
-if (todoInput) {
-
-    todoInput.addEventListener('keyup', e => {
-     let userTask = todoInput.value;
-     if (userTask === '') {
-        return false;
-      }
-     if(e.key === 'Enter' && userTask) {
-         if (!ifEditedTask) {
-             if (!todos) {
-            todos =[];
+import List from './todo-list.js';    
         
-        }    
+const todolist = new List();
+todolist.display();
         
-        let userInfor = {name: userTask, status:'pending' };
-        todos.push(userInfor);
-         } else{
-        todos[editId].name = userTask;
-        ifEditedTask = true;
+document.querySelector('#form-area').addEventListener('submit', (e) => {
+e.preventDefault();
+const activity = e.target.elements.activity.value;
+todolist.addWork(activity);
+e.target.reset();     
+  });
         
-        }
-        todoInput.value ='';
-        localStorage.setItem('my-todo', JSON.stringify(todos));
-        List.showtodo();
-    
+  document.querySelector('#delete-all').addEventListener('click', () => {
+  todolist.clearAll();
+ });
