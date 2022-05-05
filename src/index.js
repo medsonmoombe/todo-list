6fc7dd348c82b1866f59f3fb96bbe627fc207295
medsonmoombe <medsonmoombe21@gmail.com>
2022-05-05 17:23:51 +0200

    modify files for linters

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-
 import './index.css';
-import List from './todo-list.js';    
-        
+import List from './todo-list.js';
+
 const todolist = new List();
 todolist.display();
-        
+
 document.querySelector('#form-area').addEventListener('submit', (e) => {
-e.preventDefault();
-const activity = e.target.elements.activity.value;
-todolist.addWork(activity);
-e.target.reset();     
-  });
-        
-  document.querySelector('#delete-all').addEventListener('click', () => {
+  e.preventDefault();
+  const activity = e.target.elements.activity.value;
+  todolist.addWork(activity);
+  e.target.reset();
+});
+
+document.querySelector('#delete-all').addEventListener('click', () => {
   todolist.clearAll();
- });
+});
