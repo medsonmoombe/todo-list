0dbb55b12f018b881ba25d2b82a1086ef643cf6e
medsonmoombe <medsonmoombe21@gmail.com>
2022-05-05 15:51:07 +0200

    Add the functions for saving, editing and deleting todos in the list class

--- src/todo-list.js
+++ src/todo-list.js
@@ -42,5 +42,82 @@ class List {
       });
       this.activateActions();
     }
-
+    addWork(work) {
+        if (work || work === 0) {
+          const newWork = {
+            description: work,
+            completed: false,
+            index: this.list.length,
+          };
+          this.list.push(newWork);
+          this.display();
+        }
+      }
+    
+      updateActivityStatus(workIndex) {
+        if (workIndex !== undefined) {
+          if (this.list[workIndex].completed === true) {
+            this.list[workIndex].completed = false;
+          } else {
+            this.list[workIndex].completed = true;
+          }
+        }
+        this.display();
+      }
+    
+      deleteWork(workIndex) {
+        if (workIndex) {
+          this.list.splice(workIndex, 1);
+          this.display();
+        }
+      }
+    
+      clearCompletedActivity() {
+        this.list = this.list.filter((work) => work.completed === false);
+        this.display();
+      }
+    
+      clearAll() {
+        this.list.splice(0);
+        this.display();
+      }
+    
+      saveData() {
+        for (let i; i < this.list.length; i += 1) {
+          this.list[i].index = i;
+        }
+        this.list.sort((a, b) => {
+          if (a.index < b.index) return -1;
+          if (a.index > b.index) return 1;
+          return 0;
+        });
+        localStorage.setItem('todolist', JSON.stringify(this.list));
+      }
+    
+      editActivity(index, description) {
+        this.list[index].description = description;
+        this.saveData();
+      }
+    
+      activateActions() {
+        const deleteBtns = document.querySelectorAll('.delete-work');
+        if (deleteBtns) {
+          deleteBtns.forEach((work) => {
+            work.addEventListener('click', () => {
+              this.deleteWork(work.getAttribute('data'));
+            });
+          });
+        }
+        const activities = document.querySelectorAll('.work');
+        if (activities) {
+          activities.forEach((work) => {
+            work.addEventListener('input', (e) => {
+              const description = e.target.innerText;
+              const index = e.target.getAttribute('data');
+              this.editActivity(index, description);
+            });
+          });
+        }
+      }
+    
 }
